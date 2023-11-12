4b681b8efb3fb41b2a33a3169e8fd454d509d3d9
ryanmarv11 <rmarvin1@binghamton.edu>
2023-11-11 20:38:53 -0500

    hea

--- main.py
+++ main.py
@@ -1,5 +1,26 @@
 class Citizen:
-    def __init__(self, skillString):
+    def __init__(self, skillString, dreamJob):
         self.skillString = skillString
+	self.dreamJob = dreamJob
+	self.creative = int(self.skillString[0])
+	self.retail = int(self.skillString[1])
+	self.food = int(self.skillString[2])
+	self.service = int(self.skillString[3])
+	self.recreation = int(self.skillString[4])
+	self.job = None
+	self.inDreamJob = False
+
+   def assignJob(self, floor):
+	self.job = floor.name
+	if(self.job == self.dreamJob):
+		self.inDreamJob = True
+	
+class Floor:
+   def __init__(self, name):
+	self.name = name
+	self.employees = []
+
+   
+
 print("Hello, world! This is my Tiny Tower Optimizer...or at least it will be.")
 
